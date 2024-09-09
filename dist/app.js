81fe8c7c5286d2a8f6a909e419804c329f6d3170
Asadullah Islam Joy <asadullah0072002@gmail.com>
2024-09-10 00:22:51 +0600

    git ignore added

--- dist/app.js
+++ dist/app.js
@@ -3,22 +3,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const product_route_1 = require("./app/modules/product/product.route");
-const order_route_1 = require("./app/modules/order/order.route");
+const express_1 = __importDefault(require("express"));
+const user_route_1 = require("./app/modules/user/user.route");
+const service_route_1 = require("./app/modules/service/service.route");
+const slot_route_1 = require("./app/modules/slot/slot.route");
+const booking_route_1 = require("./app/modules/booking/booking.route");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.use("/api/products", product_route_1.ProductRoutes);
-app.use("/api/orders", order_route_1.OrderRoutes);
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-app.all("*", (req, res) => {
-    res.status(400).json({
+// routes
+app.use("/api/auth", user_route_1.userRoutes);
+app.use("/api/services", service_route_1.serviceRoutes);
+// app.use("/api/services", slotRoutes);
+app.use("/api/slots", slot_route_1.slotRoutes);
+app.use("/api", booking_route_1.bookingRoutes);
+const getAController = (req, res) => {
+    res.send("server is Running...");
+};
+app.get("/", getAController);
+app.use("*", (req, res) => {
+    res.status(404).json({
         success: false,
-        message: "Route is not found",
+        statusCode: 404,
+        message: "Not Found",
     });
 });
 exports.default = app;
--- dist/app/modules/order/order.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-//order interface
-Object.defineProperty(exports, "__esModule", { value: true });
